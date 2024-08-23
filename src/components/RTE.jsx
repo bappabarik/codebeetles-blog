0b74afb971ca3083cff55aa7f5acc488a56f6c5f
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2024-08-23 21:47:16 +0530

    fix: RTE content required

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -6,50 +6,61 @@ import conf from "../conf/conf";
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full text-left">
-      {label && <label className="inline-block mb-1 pl-1 dark:text-white py-2"> {label} </label>}
+      {label && (
+        <label className="inline-block mb-1 pl-1 dark:text-white py-2">
+          {label}
+        </label>
+      )}
       <Controller
         name={name || "content"}
         control={control}
-        render={({ field: { onChange } }) => (
-          <Editor
-            apiKey={conf.rteapikey}
-            initialValue={defaultValue}
-            init={{
-              initialValue: defaultValue,
-              height: 500,
-              menubar: true,
-              plugins: [
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-              ],
-              toolbar:
-                "undo redo | blocks | " +
-                "bold italic forecolor | alignleft aligncenter " +
-                "alignright alignjustify | bullist numlist outdent indent | " +
-                "removeformat | help",
-             
-              content_style: 
-              'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-
-            }}
-            onEditorChange={onChange}
-          />
+        rules={{
+          required: {
+            value: true,
+            message: "This field is required",
+          },
+          minLength: {value: 100, message: "Minimum length of the content should be 100 characters"}
+        }}
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
+          <>
+            <Editor
+              apiKey={conf.rteapikey}
+              value={value || defaultValue}
+              init={{
+                height: 500,
+                menubar: true,
+                plugins: [
+                  "advlist",
+                  "autolink",
+                  "lists",
+                  "link",
+                  "image",
+                  "charmap",
+                  "preview",
+                  "anchor",
+                  "searchreplace",
+                  "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
+                  "code",
+                  "help",
+                  "wordcount",
+                ],
+                toolbar:
+                  "undo redo | blocks | " +
+                  "bold italic forecolor | alignleft aligncenter " +
+                  "alignright alignjustify | bullist numlist outdent indent | " +
+                  "removeformat | help",
+                content_style:
+                  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+              }}
+              onEditorChange={onChange}
+            />
+            {error && <p className="text-red-600 mt-2">{error.message}</p>}
+          </>
         )}
       />
     </div>
